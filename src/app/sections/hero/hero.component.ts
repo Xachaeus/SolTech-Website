19e31bc8862eb307250745e4e3c22233332de724
Zack Sollenberger <zack@alignnx.com>
2026-06-23 15:03:55 -0400

    Fixed broken links with RouterLink

--- src/app/sections/hero/hero.component.ts
+++ src/app/sections/hero/hero.component.ts
@@ -1,10 +1,12 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { SITE } from '../../core/site.config';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
   changeDetection: ChangeDetectionStrategy.Eager,
+  imports: [RouterLink],
   styleUrl: './hero.component.css',
 })
 export class Hero {
